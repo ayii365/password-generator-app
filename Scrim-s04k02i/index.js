7303eafbae686505cf36150a38c6a45399eda2db
ayii365 <ayii365madut@gmail.com>
2026-02-17 19:41:51 +1030

    feat: Add symbols and numbers toggle

--- Scrim-s04k02i/index.js
+++ Scrim-s04k02i/index.js
@@ -1,6 +1,9 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
+const characters = {
+    allCharacters: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"],
+    alphabet: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    symbols: ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+}
 
 /* Background Mode */
 const containerEl = document.querySelector(".container");
@@ -23,34 +26,88 @@ form.addEventListener('submit', function(event) {
     const dataObject = Object.fromEntries(formData.entries());
 
     // Generate password with the given password length
-    renderPassword(dataObject.userPassLen);
+    const length = parseInt(dataObject.userPassLen); // Transform the length into an integer, as it is a string by default
+
+
+
+    const isNumbers = checkboxStatus(dataObject.isNum);
+    const isSymbols = checkboxStatus(dataObject.isSymb);
+
+    renderPassword(length, isNumbers, isSymbols);
+
+    console.log("The value of isNumbers is: " + isNumbers);
+    console.log("The value of isSymbols is: " + isSymbols);
 });
 
-function genRandNums(length) {
-    let randNums = [];
-    for (let i=0; i<length; i++){
-        randNums[i] = Math.floor(Math.random() * characters.length)
+function checkboxStatus(input) {
+    if (input === "on"){ // Checkbox was ticked
+        return true;
+    } else { // Checkbox not ticked
+        return false;
     }
+}
+
+function genRandNums(length, isNumbers, isSymbols) {
+    let randNums = [];
+
+    if (isNumbers && isSymbols) {
+        for (let i=0; i<length; i++){
+            randNums[i] = Math.floor(Math.random() * characters.allCharacters.length)
+        }
+    } else if (isNumbers && !isSymbols) {
+        for (let i=0; i<length; i++){
+            const totalCharacters = [...characters.alphabet, ...characters.numbers]
+            randNums[i] = Math.floor(Math.random() * totalCharacters.length)
+        }
+    } else if (!isNumbers && isSymbols) {
+        for (let i=0; i<length; i++){
+            const totalCharacters = [...characters.alphabet, ...characters.symbols]
+            randNums[i] = Math.floor(Math.random() * totalCharacters.length)
+        }
+    } else {
+        for (let i=0; i<length; i++){ // Alphabet only, no numbers and no symbols
+            randNums[i] = Math.floor(Math.random() * characters.alphabet.length)
+        }
+    }
+
     return randNums;
 }
 
-function generatePassword(length) {
+function generatePassword(length, isNumbers, isSymbols) {
     if (!length){ // If no password length given, use default lenth of 15
         length = 15;
     }
-    const randNums = genRandNums(length);
+    const randNums = genRandNums(length, isNumbers, isSymbols);
 
     let password = "";
-    for (let i=0; i<length; i++){
-        password += characters[ randNums[i] ]
+
+    if (isNumbers && isSymbols) {
+        for (let i=0; i<length; i++){ // All characters (numbers and symbols)
+            password += characters.allCharacters[ randNums[i] ]
+        }
+    } else if (isNumbers && !isSymbols) { // Numbers only
+        for (let i=0; i<length; i++){
+            const totalCharacters = [...characters.alphabet, ...characters.numbers]
+            password += totalCharacters[ randNums[i] ]
+        }
+    } else if (!isNumbers && isSymbols) {
+        for (let i=0; i<length; i++){ // Symbols only
+            const totalCharacters = [...characters.alphabet, ...characters.symbols]
+            password += totalCharacters[ randNums[i] ]
+        }
+    } else {
+        for (let i=0; i<length; i++){ // Alphabet only, no numbers and no symbols
+            password += characters.alphabet[ randNums[i] ]
+        }
     }
+
     return password;
 }
 
-function renderPassword(length) {
+function renderPassword(length, isNumbers, isSymbols) {
     const passSlotEl = document.querySelectorAll(".pass-slot");
     passSlotEl.forEach( slot => {
-        slot.textContent = generatePassword(length);
+        slot.textContent = generatePassword(length, isNumbers, isSymbols);
     });
     
 }
